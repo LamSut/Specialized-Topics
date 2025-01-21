942fc05e6d6d12d2a2de38f227dc40d3551dd0a8
LamSut <lamsut0007@gmail.com>
2025-01-21 19:17:01 +0700

    nil hil

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "b2111933-tfbucket"
-    key            = "b2111933-tfstate"
+    key            = "state"
     region         = "us-east-1"
     dynamodb_table = "b2111933-tftable"
   }
