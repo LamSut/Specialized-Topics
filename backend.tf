d7f76260f964cb5350ece0c2df5c616343a069d5
LamSut <lamsut0007@gmail.com>
2025-01-21 19:30:33 +0700

    test

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "b2111933-tfbucket"
-    key            = "state"
+    key            = "b2111933-tfstate"
     region         = "us-east-1"
     dynamodb_table = "b2111933-tftable"
   }
